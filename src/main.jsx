0350eb6c57eaf757e3932156036f826076f433d1
Virodula Neeraj <virodulaneeraj@gmail.com>
2024-01-29 12:54:48 +0530

    added vercel speedinsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <SpeedInsights />
   </BrowserRouter>
 );
